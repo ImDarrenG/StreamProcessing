0d979fd933f62893fcfe22eea6df107a6a56ca3d
Darren George <darrengeorge@macbookair1.home>
2015-12-16 20:29:10 +0000

    Can consume test messages
    the producer -> kafka -> consumer loop now works :)

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -1,19 +1,26 @@
 import java.util.Properties
+import java.util.Arrays
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 object Consumer extends App {
 
   val props = new Properties()
-    props.put("bootstrap.servers", "192.168.1.90:9092")
-    props.put("key.serializer",
-      "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer",
-      "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("bootstrap.servers", "192.168.1.90:9092")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "test")
 
   val consumer = new KafkaConsumer[java.lang.String, java.lang.String](props)
+  consumer.subscribe(Arrays.asList("test-topic"))
   while (true) {
     val records = consumer.poll(100)
-    for (r <- records)
-      println(r.offset() +", "+ r.key() +", "+ r.value())
+    val iterator = records.iterator()
+    while (iterator.hasNext)
+    {
+      val record = iterator.next
+      println(record.offset() +", "+
+        record.key() +", "+
+        record.value())
+    }
   }
 }
